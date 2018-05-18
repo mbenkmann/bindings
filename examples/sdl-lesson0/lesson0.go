112c0ba2a8c0fe65c012077fe4942ca67d708102
Matthias S. Benkmann <matthias@winterdrache.de>
2018-05-18 14:53:57 +0200

    Added first example (lesson 0 from https://github.com/Twinklebear/TwinklebearDev-Lessons/tree/master/Lesson0/src)

---
+++ examples/sdl-lesson0/lesson0.go
@@ -0,0 +1,25 @@
+// https://www.willusher.io/pages/sdl2/
+// https://github.com/Twinklebear/TwinklebearDev-Lessons/tree/master/Lesson0
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "runtime"
+
+    "winterdrache.de/bindings/sdl"
+)
+
+func main() {
+    runtime.LockOSThread() // SDL functions must all be called from the main thread
+
+    if sdl.Init(sdl.INIT_VIDEO) != 0 {
+        fmt.Fprintf(os.Stderr, "SDL_Init Error: %v\n", sdl.GetError())
+        os.Exit(1)
+    }
+
+    defer sdl.Quit()
+
+    os.Exit(0)
+}
