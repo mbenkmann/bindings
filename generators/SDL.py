7c4481584980debb076af0e0cd7d78af555b944e
Matthias S. Benkmann <matthias@winterdrache.de>
2018-05-28 21:38:35 +0200

    move common code from SDL.py and SDL_image.py into lib

--- generators/SDL.py
+++ generators/SDL.py
@@ -228,19 +228,7 @@ if __name__ == "__main__":
     import_idx = len(lib.out)
     lib.out.append("")
 
-    lib.describe(lib.soup.doxygen.compounddef)
-
-    lib.structs()
-    lib.unions()
-
-    for section in lib.soup.doxygen.compounddef.find_all("sectiondef"):
-        lib.out.append("")
-        lib.describe(section)
-        lib.define2const(section)
-        lib.enum2const(section)
-        lib.aliastypedefs(section)
-        lib.simpletypedefs(section)
-        lib.wrapfunctions(section)
+    lib.generate_bindings()
 
     lib.additional_boilerplate(import_idx)
 
