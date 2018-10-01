54590137bd66721f4cc351c86772b0c30ce335bb
Matthias S. Benkmann <matthias@winterdrache.de>
2018-10-02 01:30:21 +0200

    add helpful comment to cflags.go

--- sdl/cflags.go
+++ sdl/cflags.go
@@ -1,4 +1,7 @@
 package sdl
 
+// disabled#cgo CFLAGS: -D_REENTRANT -I/tmp/SDL2-2.0.9/include
+// disabled#cgo LDFLAGS: -L/tmp/SDL2-2.0.9/build/.libs -lSDL2
+
 // #cgo linux freebsd darwin pkg-config: sdl2
 import "C"
